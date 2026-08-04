62b8380ac3c18dddfe3e81d88223c2ddf65fbcd2
RickDev <henriiisilva2007@gmail.com>
2026-08-04 19:53:56 -0300

    Adicao de migration com Flyway da table assinaturas

---
+++ src/main/resources/db/migration/V1__create_table_assinaturas.sql
@@ -0,0 +1,8 @@
+ CREATE TABLE assinaturas(
+      id BIGSERIAL PRIMARY KEY,
+          servico VARCHAR(255) NOT NULL,
+          valor NUMERIC(10, 2) NOT NULL,
+          data_vencimento INT NOT NULL,
+          status VARCHAR(50) NOT NULL,
+          plano VARCHAR(100)
+ );
