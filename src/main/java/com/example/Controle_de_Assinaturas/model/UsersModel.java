70018fa8492da2930494a51988a11b52571d9467
RickDev <henriiisilva2007@gmail.com>
2026-08-09 20:47:41 -0300

    Criacao da classe UsersModel

---
+++ src/main/java/com/example/Controle_de_Assinaturas/model/UsersModel.java
@@ -0,0 +1,27 @@
+package com.example.Controle_de_Assinaturas.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@RequiredArgsConstructor
+@Entity
+@Table(name = "usuarios")
+public class UsersModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String login;
+
+    private String senha;
+
+
+}
