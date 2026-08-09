74ecf2bd12b96a526b88eb651ef8462bcb0e6b6f
RickDev <henriiisilva2007@gmail.com>
2026-08-09 20:55:33 -0300

    Criacao de migration da table usuarios e ajuste de construtores na classe UserModel

--- src/main/java/com/example/Controle_de_Assinaturas/model/UsersModel.java
+++ src/main/java/com/example/Controle_de_Assinaturas/model/UsersModel.java
@@ -7,8 +7,6 @@ import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
 @RequiredArgsConstructor
 @Entity
 @Table(name = "usuarios")
