b6d769cfb1a4fca652cb05ca801611fbe3925212
RickDev <henriiisilva2007@gmail.com>
2026-08-09 21:01:23 -0300

    Adicao das dependencias do SpringSecurity e Auth0JWT

--- src/main/java/com/example/Controle_de_Assinaturas/model/UsersModel.java
+++ src/main/java/com/example/Controle_de_Assinaturas/model/UsersModel.java
@@ -1,16 +1,14 @@
 package com.example.Controle_de_Assinaturas.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 
 @Data
 @RequiredArgsConstructor
 @Entity
 @Table(name = "usuarios")
-public class UsersModel {
+public class UsersModel{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
