cbc4d82f91bc0c24f8129cb102e7dd5384911d9c
RickDev <henriiisilva2007@gmail.com>
2026-08-12 22:17:39 -0300

    Adicao do encoder Bcrypt e do AuthenticationManager

--- src/main/java/com/example/Controle_de_Assinaturas/Security/SecurityConfig.java
+++ src/main/java/com/example/Controle_de_Assinaturas/Security/SecurityConfig.java
@@ -1,11 +1,16 @@
 package com.example.Controle_de_Assinaturas.Security;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -16,6 +21,7 @@ public class SecurityConfig {
 
     private final SecurityFilter securityFilter;
 
+    @Bean
     public SecurityFilterChain securityFilterChain (HttpSecurity http) throws Exception{
         return http
                 .csrf(AbstractHttpConfigurer::disable)
@@ -30,6 +36,15 @@ public class SecurityConfig {
                 )
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception{
+        return config.getAuthenticationManager();
     }
 
 }
