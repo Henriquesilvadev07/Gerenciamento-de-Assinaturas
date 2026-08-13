6680d88bae74f448774953852134103b83f7dba2
RickDev <henriiisilva2007@gmail.com>
2026-08-12 22:14:26 -0300

    Criacao do SecurityFilterChain para permissao de rotar e estado da aplicacao

--- src/main/java/com/example/Controle_de_Assinaturas/Security/SecurityConfig.java
+++ src/main/java/com/example/Controle_de_Assinaturas/Security/SecurityConfig.java
@@ -2,7 +2,12 @@ package com.example.Controle_de_Assinaturas.Security;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -11,6 +16,20 @@ public class SecurityConfig {
 
     private final SecurityFilter securityFilter;
 
-
+    public SecurityFilterChain securityFilterChain (HttpSecurity http) throws Exception{
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(sm ->
+                        sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(req ->
+                        req.requestMatchers("/auth/login").permitAll()
+                                .requestMatchers("/error").permitAll()
+                                .requestMatchers("/auth/register").permitAll()
+                                .requestMatchers("/", "*.html", "/css/**", "/ks/**").permitAll()
+                                .anyRequest().authenticated()
+                )
+                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
 
 }
