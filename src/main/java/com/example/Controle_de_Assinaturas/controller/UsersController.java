e9c2d5cb5b358a04cd96fdd20fa4567c5a55c703
RickDev <henriiisilva2007@gmail.com>
2026-08-15 15:48:20 -0300

    Criacao do endPoint de cadastro e efetuar login

--- src/main/java/com/example/Controle_de_Assinaturas/controller/UsersController.java
+++ src/main/java/com/example/Controle_de_Assinaturas/controller/UsersController.java
@@ -1,10 +1,43 @@
 package com.example.Controle_de_Assinaturas.controller;
 
 
+import com.example.Controle_de_Assinaturas.dto.TokenJwtDto;
+import com.example.Controle_de_Assinaturas.dto.UsersDto;
+import com.example.Controle_de_Assinaturas.model.UsersModel;
+import com.example.Controle_de_Assinaturas.service.TokenService;
+import com.example.Controle_de_Assinaturas.service.UsersService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriBuilder;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping("/auth")
+@RequiredArgsConstructor
 public class UsersController {
+
+    public final UsersService usersService;
+    private final AuthenticationManager authenticationManager;
+    private final TokenService tokenService;
+
+    public ResponseEntity efetuarLogin(@RequestBody @Valid UsersDto dto){
+        var authToken = new UsernamePasswordAuthenticationToken(dto.login(), dto.senha());
+        var authentication = authenticationManager.authenticate(authToken);
+        var JwtToken = tokenService.gerarToken((UsersModel) authentication.getPrincipal());
+        return ResponseEntity.ok(new TokenJwtDto(JwtToken));
+    }
+
+    public ResponseEntity<UsersModel> cadastro (@RequestBody @Valid UsersDto dto, UriComponentsBuilder uriBuilder){
+        var users = usersService.cadastrar(dto);
+        var uri = uriBuilder.path("/auth/registro/{id}").buildAndExpand(users.getId()).toUri();
+        return ResponseEntity.created(uri).body(users);
+    }
+
+
 }
