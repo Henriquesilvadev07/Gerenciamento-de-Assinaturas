e115f3263976f9e9c5cda7cc43f27be9b00e4350
RickDev <henriiisilva2007@gmail.com>
2026-08-15 16:28:02 -0300

    criacao da classe controller de usuarios com endpoints de deletar e atualizar por id

---
+++ src/main/java/com/example/Controle_de_Assinaturas/controller/UsersController.java
@@ -0,0 +1,31 @@
+package com.example.Controle_de_Assinaturas.controller;
+
+import com.example.Controle_de_Assinaturas.dto.UsersDto;
+import com.example.Controle_de_Assinaturas.model.UsersModel;
+import com.example.Controle_de_Assinaturas.service.UsersService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
+public class UsersController {
+
+    private final UsersService usersService;
+
+
+    @PostMapping("/{id}")
+    public ResponseEntity<UsersModel> atualizarPorId(@PathVariable Long id, @RequestBody @Valid UsersDto dto){
+        var user = usersService.atualizarPorId(id, dto);
+        return ResponseEntity.status(200).body(user);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletarPorId(@PathVariable Long id){
+        usersService.deletarPorId(id);
+        return ResponseEntity.noContent().build();
+    }
+
+}
