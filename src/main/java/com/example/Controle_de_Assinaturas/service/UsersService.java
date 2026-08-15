6f53c9e54baaccee61f27c8de7fe68bc320a7647
RickDev <henriiisilva2007@gmail.com>
2026-08-14 22:20:33 -0300

    Adicao da funcao de atualizar por Id

--- src/main/java/com/example/Controle_de_Assinaturas/service/UsersService.java
+++ src/main/java/com/example/Controle_de_Assinaturas/service/UsersService.java
@@ -4,6 +4,7 @@ import com.example.Controle_de_Assinaturas.Security.SecurityConfig;
 import com.example.Controle_de_Assinaturas.dto.UsersDto;
 import com.example.Controle_de_Assinaturas.model.UsersModel;
 import com.example.Controle_de_Assinaturas.repository.UsersRepository;
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,6 +36,14 @@ public class UsersService implements UserDetailsService {
         user.setLogin(dto.login());
         String senhaCriptografada = securityConfig.passwordEncoder().encode(dto.senha());
         user.setSenha(senhaCriptografada);
+        return usersRepository.save(user);
+    }
+
+    public UsersModel atualizarPorId(Long id, @Valid UsersDto dto){
+        var user = usersRepository.findById(id).orElseThrow(()
+        -> new EntityNotFoundException("Usuário não encontrado, tente novamente"));
+        user.setLogin(dto.login());
+        var senhaCripitografada = securityConfig.passwordEncoder().encode(dto.senha());
         return usersRepository.save(user);
     }
 
